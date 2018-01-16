b68d4b4efc301b6bc7f28ea991ac348c52c829a5
gib <gibbowden@gmail.com>
2018-01-15 20:49:39 -0600

    finished exercises

---
+++ AnotherConsoleApp/Child.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherConsoleApp
+{
+    class Child
+    {
+        public string Name { get; set; }
+        public bool Sick { get; set; }
+
+        public string ForgeASickNote()
+        {
+            return (Sick) ? $"Please excuse {Name} from class today because they are very sick." : $"{Name} is good to go.";            
+        }
+
+    }
+}
--- AnotherConsoleApp/Program.cs
+++ AnotherConsoleApp/Program.cs
@@ -10,7 +10,32 @@ namespace AnotherConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is our second time about talking about the same shit");
+            var children = new List<Child>
+            {
+                new Child { Name = "Suzy", Sick = false },
+                new Child { Name = "Ralph", Sick = false } 
+            };
+
+            foreach (var child in children)
+            {
+                Console.WriteLine($"The kid's name is {child.Name}, and she is {(child.Sick == true ? "sick" : "healthy")}");
+                Console.WriteLine(child.ForgeASickNote());
+            }
+
+
+
+            //var suzy = new Child { Name = "Suzy", Sick = false };
+            //var ralph = new Child { Name = "Ralph", Sick = false };
+
+            //children.Add(suzy);
+            //children.Add(ralph);
+
+
+
+            var lineRead = Console.ReadLine(); 
+
+
+
             
         }
     }
