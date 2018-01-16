324d5db9a52e486a0c3ad49ad2136c94d812d96c
gib <gibbowden@gmail.com>
2018-01-15 18:37:43 -0600

    Add project files.

---
+++ AnotherConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is our second time about talking about the same shit");
+            
+        }
+    }
+}
